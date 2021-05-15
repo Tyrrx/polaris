93a79cf11758d2b7ae336840427ed7d0c168c7d7
David Retzlaff <david.r3tzlaff@gmail.com>
2021-05-15 16:30:10 +0200

    change constructor to private; move static functions to end

--- src/main/java/polaris/Result.java
+++ src/main/java/polaris/Result.java
@@ -21,7 +21,7 @@ public abstract class Result<T> {
 
     private final boolean isSuccess;
 
-    public Result(boolean isSuccess) {
+    private Result(boolean isSuccess) {
         this.isSuccess = isSuccess;
     }
 
@@ -40,13 +40,74 @@ public abstract class Result<T> {
     }
 
     /**
+     * Matches a result's state (success, failure) without a return value.
+     * @param success Consumer<T> executed on success
+     * @param failure Consumer<T> executed on failure
+     */
+    public abstract void matchVoid(Consumer<T> success, Consumer<String> failure);
+
+    /**
+     * Matches a result's state (success, failure) and returns a value of type T1.
+     * @param success Function<T, T1> executed on success
+     * @param failure Function<T, T1> executed on failure
+     * @param <T1>    return value type
+     * @return T1
+     */
+    public abstract <T1> T1 match(Function<T, T1> success, Function<String, T1> failure);
+
+    /**
+     * Binds a result with a possible failure to an existing result if the existing result was successful.
+     * @param binder is a method that returns the result to bind
+     * @param <T1>   is the type of the bind transformation
+     * @return a result that contains the value and state of the binding
+     */
+    public <T1> Result<T1> bind(Function<T, Result<T1>> binder) {
+        return match(
+            binder::apply,
+            Result::failure);
+    }
+
+    /**
+     * Maps an existing value to a new Polaris.Result<T1> with value type T1.
+     * @param mapper Function<T, T1> transforms value on success
+     * @param <T1>   New value type
+     * @return Polaris.Result<T1>
+     */
+    public <T1> Result<T1> map(Function<T, T1> mapper) {
+        return this.bind((value) -> Result.success(mapper.apply(value)));
+    }
+
+    /**
+     * Transforms a Result of type T to an Optional of type T.
+     * @return an empty Optional on failure and a not empty Optional on success
+     */
+    public Optional<T> toOptional() {
+        return match(
+            Optional::of,
+            failure -> Optional.empty());
+    }
+
+    public T getValueOrDefault(T defaultValue) {
+        return match(
+            success -> success,
+            failure -> defaultValue);
+    }
+
+    public String getErrorOrDefault() {
+        return match(
+            success -> "",
+            failure -> failure
+        );
+    }
+
+    /**
      * Aggregates results from a stream to one result with a list of all values.
      * Enumerates all elements in stream
      *
      * @param stream         result stream
      * @param errorSeparator separator between error messages
      * @param <TResult>            is the type of the aggregation
-     * @return 
+     * @return
      */
     public static <TResult> Result<List<TResult>> aggregate(Stream<Result<TResult>> stream, String errorSeparator) {
         StringBuffer stringBuffer = new StringBuffer();
@@ -122,66 +183,5 @@ public abstract class Result<T> {
 
     public static <TResult> CompletableFuture<Stream<Result<TResult>>> chooseAsync(Stream<CompletableFuture<Result<TResult>>> completableFutureStream, Consumer<String> errorHandler) {
         return CompletableFuture.supplyAsync(() -> Result.choose(completableFutureStream.map(CompletableFuture::join), errorHandler));
-    }
-
-    /**
-     * Matches a result's state (success, failure) without a return value.
-     * @param success Consumer<T> executed on success
-     * @param failure Consumer<T> executed on failure
-     */
-    public abstract void matchVoid(Consumer<T> success, Consumer<String> failure);
-
-    /**
-     * Matches a result's state (success, failure) and returns a value of type T1.
-     * @param success Function<T, T1> executed on success
-     * @param failure Function<T, T1> executed on failure
-     * @param <T1>    return value type
-     * @return T1
-     */
-    public abstract <T1> T1 match(Function<T, T1> success, Function<String, T1> failure);
-
-    /**
-     * Binds a result with a possible failure to an existing result if the existing result was successful.
-     * @param binder is a method that returns the result to bind
-     * @param <T1>   is the type of the bind transformation
-     * @return a result that contains the value and state of the binding
-     */
-    public <T1> Result<T1> bind(Function<T, Result<T1>> binder) {
-        return match(
-            binder::apply,
-            Result::failure);
-    }
-
-    /**
-     * Maps an existing value to a new Polaris.Result<T1> with value type T1.
-     * @param mapper Function<T, T1> transforms value on success
-     * @param <T1>   New value type
-     * @return Polaris.Result<T1>
-     */
-    public <T1> Result<T1> map(Function<T, T1> mapper) {
-        return this.bind((value) -> Result.success(mapper.apply(value)));
-    }
-
-    /**
-     * Transforms a Result of type T to an Optional of type T.
-     * @return an empty Optional on failure and a not empty Optional on success
-     */
-    public Optional<T> toOptional() {
-        return match(
-            Optional::of,
-            failure -> Optional.empty());
-    }
-
-    public T getValueOrDefault(T defaultValue) {
-        return match(
-            success -> success,
-            failure -> defaultValue);
-    }
-
-    public String getErrorOrDefault() {
-        return match(
-            success -> "",
-            failure -> failure
-        );
     }
 }
